6d8978551d6016d12f1d4a3bfb6a48f87cc09f10
Rizk Gendy <80922036+rizk-gendy@users.noreply.github.com>
2022-03-26 15:00:49 +0200

    Add files via upload

---
+++ components/CartIem/index.js
@@ -0,0 +1,34 @@
+import { View, Text ,ImageBackground } from "react-native";
+import React from "react";
+import styles from "./styles";
+import StyleButton from "../StyleButton";
+const image = require("../../assets//ModelX.jpeg");
+
+
+const CarItem = () => {
+  return (
+    <View style={styles.carContainer}>
+      <ImageBackground style={styles.image} source={image} />
+      <View style={styles.titles}>
+        <Text style={styles.title}>Model S</Text>
+        <Text style={styles.subtitle}>Starting at $ 69,420</Text>
+      </View>
+      <StyleButton 
+      type="primary" 
+      content={"Custom Order"} 
+      onPress={()=>{
+        console.log('Custom order button was pressed ')
+      }}
+      />
+      <StyleButton 
+      type="secondary" 
+      content={"Existing Inventory "} 
+      onPress={()=>{
+        console.log('Existing Inventory button was pressed ')
+      }}
+      />
+    </View>
+  );
+};
+
+export default CarItem;
